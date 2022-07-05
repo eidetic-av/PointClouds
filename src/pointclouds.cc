97848eeb13ebab2561d6d656aa6414a4c925cb80
Matt Hughes <matth@eidetic.net.au>
2022-07-05 17:07:54 +1000

    Fix pointer arithmetic, remove "this->"

--- src/pointclouds.cc
+++ src/pointclouds.cc
@@ -13,8 +13,8 @@ namespace bob::types {
   using namespace std::chrono;
 
   void PointCloud::resize(std::size_t size) {
-    this->positions.resize(size);
-    this->colors.resize(size);
+    positions.resize(size);
+    colors.resize(size);
   }
 
   auto PointCloud::serialize(bool compress) -> bytes {
@@ -30,15 +30,15 @@ namespace bob::types {
       // buffer will be in the Draco 3D format
       using namespace draco;
       draco::PointCloudBuilder draco_builder;
-      draco_builder.Start(this->size());
+      draco_builder.Start(size());
       auto pos_attribute_id = draco_builder.AddAttribute(
 	  PointAttribute::POSITION, 3, DataType::DT_INT16);
       draco_builder.SetAttributeValuesForAllPoints(
-	  pos_attribute_id, this->positions.data(), sizeof(short3));
+	  pos_attribute_id, positions.data(), sizeof(short3));
       auto col_attribute_id = draco_builder.AddAttribute(
 	  PointAttribute::COLOR, 4, DataType::DT_UINT8);
       draco_builder.SetAttributeValuesForAllPoints(
-	  col_attribute_id, this->colors.data(), sizeof(color));
+	  col_attribute_id, colors.data(), sizeof(color));
       // Finalize() below takes a bool specifying if we should run a
       // deduplication step. It's generally too slow to use in real-time
       auto draco_point_cloud = draco_builder.Finalize(false);
@@ -54,23 +54,18 @@ namespace bob::types {
       packet.data.assign(buffer_ptr, buffer_ptr + out_buffer.size());
     } else {
       // if we don't want to compress our point-cloud, the internal data
-      // buffer will contain first the point-count, then the vector of point
-      // positions, then the vector of point colors
-      auto point_count = this->size();
+      // buffer will contain first the positions, then the colors
       auto positions_size = sizeof(short3) * point_count;
       auto colors_size = sizeof(color) * point_count;
-      auto internal_data_size = sizeof(unsigned long) + positions_size + colors_size;
-      packet.data.resize(internal_data_size);
-      std::memcpy(packet.data.data(), &point_count, sizeof(unsigned long));
-      std::memcpy(packet.data.data() + sizeof(unsigned long), this->positions.data(),
-		  positions_size);
-      std::memcpy(packet.data.data() + sizeof(unsigned long) + positions_size,
-		  this->colors.data(), colors_size);
+      packet.data.resize(positions_size + colors_size);
+      memcpy(packet.data.data(), positions.data(), positions_size);
+      memcpy(packet.data.data() + positions_size, colors.data(), colors_size);
     }
     // now the packet is initialised with either raw or draco data, we serialize it
     // using zpp_bits (because zpp_bits is really fast)
     auto [output_data, zpp_serialize] = zpp::bits::data_out();
     zpp_serialize(packet).or_throw();
+
     return output_data;
   }
 
@@ -97,23 +92,18 @@ namespace bob::types {
       auto draco_positions = draco_point_cloud->GetAttributeByUniqueId(positions_attr_id);
       auto draco_colors = draco_point_cloud->GetAttributeByUniqueId(colors_attr_id);
       // copy point data from the draco type into our PointCloud
-      auto point_count = draco_positions->size();
-      auto positions_size = sizeof(short3) * point_count;
-      auto colors_size = sizeof(color) * point_count;
       auto input_positions_ptr = std::bit_cast<short3*>(draco_positions->buffer()->data());
       auto input_colors_ptr = std::bit_cast<color*>(draco_colors->buffer()->data());
-      point_cloud.positions.assign(input_positions_ptr, input_positions_ptr + positions_size);
-      point_cloud.colors.assign(input_colors_ptr, input_colors_ptr + colors_size);
+      point_cloud.positions.assign(input_positions_ptr, input_positions_ptr + point_count);
+      point_cloud.colors.assign(input_colors_ptr, input_colors_ptr + point_count);
     } else {
       // it it's not compressed, our encoding is first the point count, then
       // the vectors of positions and colors
-      auto point_count = *std::bit_cast<unsigned long *>(buffer.data());
       auto positions_size = sizeof(short3) * point_count;
-      auto colors_size = sizeof(color) * point_count;
-      auto input_positions_ptr = std::bit_cast<short3 *>(buffer.data() + sizeof(unsigned long));
-      auto input_colors_ptr = std::bit_cast<color *>(buffer.data() + sizeof(unsigned long) + positions_size);
-      point_cloud.positions.assign(input_positions_ptr, input_positions_ptr + positions_size);
-      point_cloud.colors.assign(input_colors_ptr, input_colors_ptr + colors_size);
+      auto input_positions = std::bit_cast<short3 *>(packet.data.data());
+      auto input_colors = std::bit_cast<color *>(packet.data.data() + positions_size);
+      point_cloud.positions.assign(input_positions, input_positions + point_count);
+      point_cloud.colors.assign(input_colors, input_colors + point_count);
     }
     return point_cloud;
   }
